f523ebb46c274103ea4ce8fb32ebf93fed201d2f
bblodfon <bblodfon@gmail.com>
2015-02-08 18:56:06 +0200

    Added some info for the barber.pl file

--- barber.pl
+++ barber.pl
@@ -1,6 +1,8 @@
+% try: shaves(barber,john).
+% and to get Russel's Paradox: shaves(barber,barber)
+
 shaves(barber, X) :- male(X), \+ shaves(X,X).
 male(barber).
 male(john).
 male(akon).
 male(eminem).
-male(malakas).
