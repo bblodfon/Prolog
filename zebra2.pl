ebd702aee56217ce1074987d297052c757fa0826
bblodfon <bblodfon@gmail.com>
2015-02-11 00:06:18 +0200

    Re-written zebra.pl for more "visual" output

--- zebra2.pl
+++ zebra2.pl
@@ -1,6 +1,7 @@
 % My own solution to "Who owns the zebra and who drinks the pepsi(!) puzzle" 
-% Run: zebra(X).
+% Run: zebra(X). or simply: solve.
 
+solve		 :- zebra(_).
 zebra(Houses):- Houses = [house(norwegian,_,_,_,_),house(_,blue,_,_,_),house(_,_,_,milk,_),_,_],
 				member(house(englishman,red,_,_,_),Houses),
 				member(house(spaniard,_,dog,_,_),Houses),
@@ -12,10 +13,12 @@ zebra(Houses):- Houses = [house(norwegian,_,_,_,_),house(_,blue,_,_,_),house(_,_
 				next_to(house(_,_,_,_,chesterfields),house(_,_,fox,_,_),Houses),
 				next_to(house(_,_,_,_,kools),house(_,_,horse,_,_),Houses) ,
 				member(house(_,_,_,orange_juice,lucky_strike),Houses),
-				member(house(japanese,_,_,_,parliaments),Houses).
+				member(house(japanese,_,_,_,parliaments),Houses),
+				member(house(WhoOwnsTheZebra,_,zebra,_,_),Houses), print('The '), print(WhoOwnsTheZebra), print(' has the zebra!!!'), nl,
+				member(house(WhoOwnsThePepsi,_,_,pepsi,_),Houses), print('The '), print(WhoOwnsThePepsi), print(' has the pepsi-cola!!!'),!.
 right_of(X,Y,[X,Y|_]).
 right_of(X,Y,[_,X,Y|_]).
 right_of(X,Y,[_,_,X,Y|_]).
 right_of(X,Y,[_,_,_,X,Y]).
-next_to(X,Y, Houses ):-right_of(X,Y,Houses).
+next_to(X,Y, Houses):-right_of(X,Y,Houses).
 next_to(X,Y,Houses):-right_of(Y,X,Houses).
