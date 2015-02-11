69e5f2ffd925b58220771e42b66210f3408cf676
bblodfon <bblodfon@gmail.com>
2015-02-12 00:28:10 +0200

    Added info
    - info for better understanding of smooth2.pl mostly and smooth.pl also

--- smooth.pl
+++ smooth.pl
@@ -1,4 +1,8 @@
-% prime_factors(N, L) :- N is the list of prime factors of N.
+% This program evaluates the number of B-smooth numbers from I to J.
+% The prime_factors precidate is from the 99 prolog problems i think!
+% Run: smooth(2,1,40,L).
+
+% prime_factors(N, L) :- L is the list of prime factors of N.
 %    (integer,list) (+,?)
 
 prime_factors(N,L) :- N > 0,  prime_factors(N,L,2).
@@ -8,10 +12,9 @@ prime_factors(N,L) :- N > 0,  prime_factors(N,L,2).
 
 prime_factors(1,[],_) :- !.
 prime_factors(N,[F|L],F) :-                           % N is multiple of F
-   R is N // F, N =:= R * F, !, prime_factors(R,L,F).
+   R is N // F, N =:= R * F, !, prime_factors(R,L,F). % N = R * F + ( mod = 0 )
 prime_factors(N,L,F) :- 
    next_factor(N,F,NF), prime_factors(N,L,NF).        % N is not multiple of F
-   
 
 % next_factor(N,F,NF) :- when calculating the prime factors of N
 %    and if F does not divide N then NF is the next larger candidate to
@@ -21,15 +24,14 @@ next_factor(_,2,3) :- !.
 next_factor(N,F,NF) :- F * F < N, !, NF is F + 2.
 next_factor(N,_,N).                                 % F > sqrt(N)
 
-
 lasti([A],A).
 lasti([_|T],N):-lasti(T,N).
 
 bigger(I,N) :- prime_factors(I,L) , lasti(L,N).
 
-sm(_,1,1).% opoiodipote B kai na paro o ena einai smooth!!!!
-sm(B,I,1) :- bigger(I,L) , L =< B.  % einai B-smooth o I
-sm(B,I,0) :- bigger(I,L) , L > B.     % den einai B-smooth o I
+sm(_,1,1).                          % 1 is always smooth!!!!
+sm(B,I,1) :- bigger(I,L) , L =< B.  % I is B-smooth
+sm(B,I,0) :- bigger(I,L) , L > B.   % I is not B-smooth
 
 smooth(B,I,J,N) :- I = J , X = I , sm(B,X,N).
-smooth(B,I,J,N) :- I =< J , sm(B,I,N1) , Y is I+1 , smooth(B,Y,J,N2) , N is N1+N2.
+smooth(B,I,J,N) :- I < J , sm(B,I,N1) , Y is I+1 , smooth(B,Y,J,N2) ,!,  N is N1+N2.
--- smooth2.pl
+++ smooth2.pl
@@ -1,14 +1,24 @@
+% This program calculates the same thing as smooth.pl but
+% with an ingenious (yet strange) way! (it was made by a classmate of mine
+% and it has stuck in my head ever since)
+% Run: smooth(2,1,40,L).
+
+smooth(B,I,J,A):- primes(3,B,[2],L),node(L,1,I,J,A),!.
+
+% primes returns a list (L) with all the primes =< Telos (B).
+% Should always be called like this: primes(3,B,[2],L).
+primes(I,Telos,P,P) :- I > Telos,!.
+primes(I,Telos,P,Primes) :- I1 is I+2 ,(check(P,I) -> append(P,[I],K) , primes(I1,Telos,K,Primes) ; primes(I1,Telos,P,Primes)).
 check([],_).
 check([H|T],A):- A mod H > 0 , check(T,A). 
 
-primes(I,Telos,P,P) :- I > Telos,!.
-primes(I,Telos,P,Primes) :- I1 is I+2 ,(check(P,I) -> append(P,[I],K) , primes(I1,Telos,K,Primes) ; primes(I1,Telos,P,Primes)).
-
-add_smooths([],_,_,_,0).
-add_smooths([H|T],A,I,J,N):- add_smooths(T,A,I,J,N2),A1 is A*H ,node([H|T],A1,I,J,N1) ,N is N1+N2. 
-
+% node predicate builds from a list of primes (all<B from primes predicate) all possible combinations
+% of numbers from these prime factors and counts the ones that are inside the [I,J] interval.
+% e.g. for B=5: primes will return L=[2,3,5] and node will keep only 6 of the combinations:
+% 10=2x5,16=2^4,18=2x3^2,12=3x2^2,15=3x5,20=2^2x5, all build with prime factors<B, so B-smooth!
 node(_,A,_,J,0):-A>J,!.
 node(P,A,I,J,S) :- A<I , add_smooths(P,A,I,J,S). 
-node(P,A,I,J,S) :- A>=I,A=<J , add_smooths(P,A,I,J,S1) ,S is S1+1.
-
-smooth(B,I,J,A):-primes(3,B,[2],L),node(L,1,I,J,A),!.
+node(P,A,I,J,S) :- A>=I,A=<J ,% print(A), nl,
+					add_smooths(P,A,I,J,S1) ,S is S1+1.
+add_smooths([],_,_,_,0).
+add_smooths([H|T],A,I,J,N):- add_smooths(T,A,I,J,N2),A1 is A*H , node([H|T],A1,I,J,N1) ,N is N1+N2.
